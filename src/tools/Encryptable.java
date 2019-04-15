752e90e4b5b260c3c5308afe4c0b247ea9a3e4ea
David Eduardo Vazquez Vargas <vvdavided@gmail.com>
2019-04-15 14:33:56 -0500

    Create Encryptable.java

---
+++ src/tools/Encryptable.java
@@ -0,0 +1,25 @@
+package tools;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public interface Encryptable {
+
+    public default String encryptPass(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
