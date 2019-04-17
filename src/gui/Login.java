5e1e01009a07b064cb49c7f9d3f9935a81db1b5f
David Vazquez <vvdavided@gmail.com>
2019-04-17 14:44:05 -0500

    Update login

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -9,25 +9,34 @@ public class Login extends javax.swing.JFrame {
     public Login() {
         initComponents();
         setLocationRelativeTo(null);
+        
+         TextPassword.setEchoChar((char)0);
+         TextPassword.setText("Contraseña");
+         TextPassword.setForeground(Color.GRAY);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        BotonLog = new javax.swing.JButton();
         BotonOlvidado = new javax.swing.JButton();
+        BotonLog = new javax.swing.JButton();
         TextUsuario = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         TextPassword = new javax.swing.JPasswordField();
+        jMenuBar2 = new javax.swing.JMenuBar();
+        jMenu3 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu4 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Inicio de sesión");
         setResizable(false);
 
-        BotonLog.setText("Ingresar");
-
         BotonOlvidado.setText("Olvidé mi contraseña");
+
+        BotonLog.setText("Ingresar");
 
         TextUsuario.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         TextUsuario.setForeground(new java.awt.Color(153, 153, 153));
@@ -50,6 +59,7 @@ public class Login extends javax.swing.JFrame {
         jLabel1.setText("Bienvenido, inicia sesión para continuar.");
 
         TextPassword.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        TextPassword.setText("Contraseña");
         TextPassword.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 TextPasswordFocusGained(evt);
@@ -58,6 +68,28 @@ public class Login extends javax.swing.JFrame {
                 TextPasswordFocusLost(evt);
             }
         });
+
+        jMenu3.setText("Archivo");
+
+        jMenuItem2.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F4, java.awt.event.InputEvent.ALT_MASK));
+        jMenuItem2.setText("Salir");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jMenuItem2);
+
+        jMenuBar2.add(jMenu3);
+
+        jMenu4.setText("Ayuda");
+
+        jMenuItem3.setText("Acerca de...");
+        jMenu4.add(jMenuItem3);
+
+        jMenuBar2.add(jMenu4);
+
+        setJMenuBar(jMenuBar2);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -78,9 +110,9 @@ public class Login extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(21, 21, 21)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(15, 15, 15)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
                 .addComponent(TextUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(TextPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -88,7 +120,7 @@ public class Login extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(BotonOlvidado)
                     .addComponent(BotonLog))
-                .addContainerGap(27, Short.MAX_VALUE))
+                .addContainerGap(18, Short.MAX_VALUE))
         );
 
         TextUsuario.getAccessibleContext().setAccessibleName("");
@@ -100,7 +132,7 @@ public class Login extends javax.swing.JFrame {
         if (TextUsuario.getText().equals("Usuario") && TextUsuario.getForeground() == Color.GRAY) {
             TextUsuario.setText("");
             TextUsuario.setForeground(Color.BLACK);
-        }
+        } 
     }//GEN-LAST:event_TextUsuarioFocusGained
 
     private void TextUsuarioFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_TextUsuarioFocusLost
@@ -127,8 +159,12 @@ public class Login extends javax.swing.JFrame {
             TextPassword.setEchoChar((char) 0);
             TextPassword.setText("Contraseña");
             TextPassword.setForeground(Color.GRAY);
-        }
+        }   
     }//GEN-LAST:event_TextPasswordFocusLost
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     public static void main(String args[]) {
         SwingUtilities.invokeLater(new Runnable() {
@@ -145,6 +181,11 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JPasswordField TextPassword;
     private javax.swing.JTextField TextUsuario;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenuBar jMenuBar2;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     // End of variables declaration//GEN-END:variables
     
     private class LoginDriver {
