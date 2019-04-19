ae6e991434f414e081607d0a5f03eeb5a6144529
David Vazquez <vvdavided@gmail.com>
2019-04-18 22:01:32 -0500

    Update Login

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -1,29 +1,44 @@
 package gui;
 
-import com.alee.laf.WebLookAndFeel;
+import com.alee.extended.layout.VerticalFlowLayout;
+import com.alee.extended.window.WebPopOver;
+import com.alee.laf.label.WebLabel;
+import com.alee.laf.optionpane.WebOptionPane;
 import java.awt.Color;
-import javax.swing.SwingUtilities;
+import javax.swing.JMenuItem;
 
 public class Login extends javax.swing.JFrame {
+
+    private WebPopOver popOver;
 
     public Login() {
         initComponents();
         setLocationRelativeTo(null);
         
-         TextPassword.setEchoChar((char)0);
-         TextPassword.setText("Contraseña");
-         TextPassword.setForeground(Color.GRAY);
+        //inicializa passwordJPF a vacio
+        passwordJPF.setEchoChar((char) 0);
+        passwordJPF.setText("Contraseña");
+        passwordJPF.setForeground(Color.GRAY);
+
+        //Inicializar pop-up
+        popOver = new WebPopOver();
+
+        popOver.setMargin(10);
+        popOver.setCloseOnFocusLoss(true);
+        popOver.setMovable(false);
+        popOver.setLayout(new VerticalFlowLayout());
+        popOver.add(new WebLabel("Contacta al administrador para recuperar el acceso"));
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        BotonOlvidado = new javax.swing.JButton();
-        BotonLog = new javax.swing.JButton();
-        TextUsuario = new javax.swing.JTextField();
+        olvideJB = new javax.swing.JButton();
+        ingresarJB = new javax.swing.JButton();
+        usuarioJTF = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
-        TextPassword = new javax.swing.JPasswordField();
+        passwordJPF = new javax.swing.JPasswordField();
         jMenuBar2 = new javax.swing.JMenuBar();
         jMenu3 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
@@ -34,38 +49,48 @@ public class Login extends javax.swing.JFrame {
         setTitle("Inicio de sesión");
         setResizable(false);
 
-        BotonOlvidado.setText("Olvidé mi contraseña");
-
-        BotonLog.setText("Ingresar");
-
-        TextUsuario.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        TextUsuario.setForeground(new java.awt.Color(153, 153, 153));
-        TextUsuario.setToolTipText("");
-        TextUsuario.addFocusListener(new java.awt.event.FocusAdapter() {
-            public void focusGained(java.awt.event.FocusEvent evt) {
-                TextUsuarioFocusGained(evt);
-            }
-            public void focusLost(java.awt.event.FocusEvent evt) {
-                TextUsuarioFocusLost(evt);
+        olvideJB.setText("Olvidé mi contraseña");
+        olvideJB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                olvideJBActionPerformed(evt);
             }
         });
-        TextUsuario.addActionListener(new java.awt.event.ActionListener() {
+
+        ingresarJB.setText("Ingresar");
+        ingresarJB.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                TextUsuarioActionPerformed(evt);
+                ingresarJBActionPerformed(evt);
+            }
+        });
+
+        usuarioJTF.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        usuarioJTF.setForeground(new java.awt.Color(153, 153, 153));
+        usuarioJTF.setToolTipText("");
+        usuarioJTF.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                usuarioJTFFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                usuarioJTFFocusLost(evt);
             }
         });
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel1.setText("Bienvenido, inicia sesión para continuar.");
 
-        TextPassword.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        TextPassword.setText("Contraseña");
-        TextPassword.addFocusListener(new java.awt.event.FocusAdapter() {
+        passwordJPF.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        passwordJPF.setText("Contraseña");
+        passwordJPF.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
-                TextPasswordFocusGained(evt);
+                passwordJPFFocusGained(evt);
             }
             public void focusLost(java.awt.event.FocusEvent evt) {
-                TextPasswordFocusLost(evt);
+                passwordJPFFocusLost(evt);
+            }
+        });
+        passwordJPF.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                passwordJPFActionPerformed(evt);
             }
         });
 
@@ -84,7 +109,13 @@ public class Login extends javax.swing.JFrame {
 
         jMenu4.setText("Ayuda");
 
+        jMenuItem3.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F1, 0));
         jMenuItem3.setText("Acerca de...");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
         jMenu4.add(jMenuItem3);
 
         jMenuBar2.add(jMenu4);
@@ -99,12 +130,12 @@ public class Login extends javax.swing.JFrame {
                 .addGap(24, 24, 24)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(BotonOlvidado)
+                        .addComponent(olvideJB)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(BotonLog))
+                        .addComponent(ingresarJB))
                     .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(TextUsuario)
-                    .addComponent(TextPassword))
+                    .addComponent(usuarioJTF)
+                    .addComponent(passwordJPF))
                 .addContainerGap(24, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -113,86 +144,98 @@ public class Login extends javax.swing.JFrame {
                 .addGap(15, 15, 15)
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
-                .addComponent(TextUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(usuarioJTF, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(TextPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(passwordJPF, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(BotonOlvidado)
-                    .addComponent(BotonLog))
-                .addContainerGap(18, Short.MAX_VALUE))
+                    .addComponent(olvideJB)
+                    .addComponent(ingresarJB))
+                .addContainerGap(19, Short.MAX_VALUE))
         );
 
-        TextUsuario.getAccessibleContext().setAccessibleName("");
+        usuarioJTF.getAccessibleContext().setAccessibleName("");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void TextUsuarioFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_TextUsuarioFocusGained
-        if (TextUsuario.getText().equals("Usuario") && TextUsuario.getForeground() == Color.GRAY) {
-            TextUsuario.setText("");
-            TextUsuario.setForeground(Color.BLACK);
-        } 
-    }//GEN-LAST:event_TextUsuarioFocusGained
-
-    private void TextUsuarioFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_TextUsuarioFocusLost
-        if (TextUsuario.getText().equals("")) {
-            TextUsuario.setText("Usuario");
-            TextUsuario.setForeground(Color.GRAY);
+    private void usuarioJTFFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_usuarioJTFFocusGained
+        if (usuarioJTF.getText().equals("Usuario") && usuarioJTF.getForeground() == Color.GRAY) {
+            usuarioJTF.setText("");
+            usuarioJTF.setForeground(Color.BLACK);
         }
-    }//GEN-LAST:event_TextUsuarioFocusLost
+    }//GEN-LAST:event_usuarioJTFFocusGained
 
-    private void TextUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextUsuarioActionPerformed
-
-    }//GEN-LAST:event_TextUsuarioActionPerformed
-
-    private void TextPasswordFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_TextPasswordFocusGained
-        if (TextPassword.getText().equals("Contraseña") && TextPassword.getForeground() == Color.GRAY) {
-            TextPassword.setEchoChar('*');
-            TextPassword.setText("");
-            TextPassword.setForeground(Color.BLACK);
+    private void usuarioJTFFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_usuarioJTFFocusLost
+        if (usuarioJTF.getText().equals("")) {
+            usuarioJTF.setText("Usuario");
+            usuarioJTF.setForeground(Color.GRAY);
         }
-    }//GEN-LAST:event_TextPasswordFocusGained
+    }//GEN-LAST:event_usuarioJTFFocusLost
 
-    private void TextPasswordFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_TextPasswordFocusLost
-        if (TextPassword.getText().equals("")) {
-            TextPassword.setEchoChar((char) 0);
-            TextPassword.setText("Contraseña");
-            TextPassword.setForeground(Color.GRAY);
-        }   
-    }//GEN-LAST:event_TextPasswordFocusLost
+    private void passwordJPFFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_passwordJPFFocusGained
+        if (passwordJPF.getText().equals("Contraseña") && passwordJPF.getForeground() == Color.GRAY) {
+            passwordJPF.setEchoChar('*');
+            passwordJPF.setText("");
+            passwordJPF.setForeground(Color.BLACK);
+        }
+    }//GEN-LAST:event_passwordJPFFocusGained
+
+    private void passwordJPFFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_passwordJPFFocusLost
+        if (passwordJPF.getText().equals("")) {
+            passwordJPF.setEchoChar((char) 0);
+            passwordJPF.setText("Contraseña");
+            passwordJPF.setForeground(Color.GRAY);
+        }
+    }//GEN-LAST:event_passwordJPFFocusLost
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
         System.exit(0);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
-    public static void main(String args[]) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                WebLookAndFeel.install();
-                new Login().setVisible(true);
-            }
-        });
+    private void olvideJBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_olvideJBActionPerformed
+        popOver.show(olvideJB);
+    }//GEN-LAST:event_olvideJBActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        //Mostrar en título el texto del JMenuItem
+        WebOptionPane.showMessageDialog(null, "Cuestion 0.1",
+                ((JMenuItem) evt.getSource()).getText(),
+                WebOptionPane.INFORMATION_MESSAGE);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+    private void passwordJPFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordJPFActionPerformed
+        ingresar();
+    }//GEN-LAST:event_passwordJPFActionPerformed
+
+    private void ingresarJBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ingresarJBActionPerformed
+        ingresar();
+    }//GEN-LAST:event_ingresarJBActionPerformed
+
+    private void ingresar() {
+        throw new UnsupportedOperationException("Not implemented, yet");
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton BotonLog;
-    private javax.swing.JButton BotonOlvidado;
-    private javax.swing.JPasswordField TextPassword;
-    private javax.swing.JTextField TextUsuario;
+    private javax.swing.JButton ingresarJB;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar2;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JButton olvideJB;
+    private javax.swing.JPasswordField passwordJPF;
+    private javax.swing.JTextField usuarioJTF;
     // End of variables declaration//GEN-END:variables
-    
+
     private class LoginDriver {
+
         private Login v;
+
         public LoginDriver(Login v) {
             this.v = v;
         }
-        
+
     }
 }
