83c7489ec739bdada3b15b7eb9d18041cc90eba3
David Vazquez <vvdavided@gmail.com>
2019-04-20 02:23:20 -0500

    Move encrypt method and update openJFrame

--- src/tools/GUITools.java
+++ src/tools/GUITools.java
@@ -1,11 +1,34 @@
 package tools;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import javax.swing.JFrame;
 
 public class GUITools {
 
-    public static void openJFrame(JFrame old, JFrame newOne) {
-        old.setVisible(false);
-        newOne.setVisible(true);
+    public static void openJFrame(JFrame old, JFrame next) {
+        next.setLocationRelativeTo(old);
+        if (old != null) {
+            old.setVisible(false);
+        }
+        next.setVisible(true);
     }
+
+    public static String encrypt(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
