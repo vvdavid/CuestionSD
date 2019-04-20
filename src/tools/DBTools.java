768dcd06081c782d08604aacb6145c5e8caabce0
David Vazquez <vvdavided@gmail.com>
2019-04-20 02:07:29 -0500

    Create DBTools.java

---
+++ src/tools/DBTools.java
@@ -0,0 +1,27 @@
+package tools;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBTools {
+
+    public static String path = System.getenv("APPDATA") + File.separatorChar + "Cuestion" + File.separatorChar + "cuestion_data";
+
+    // única instancia de Connection
+    private static Connection con;
+
+    public static Connection getConnection() {
+        if (con == null) {
+            try {
+                con = DriverManager.getConnection("jdbc:sqlite:" + path);
+            } catch (SQLException ex) {
+                System.err.println(ex);
+            }
+        }
+        return con;
+    }
+}
