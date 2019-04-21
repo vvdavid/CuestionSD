3b90606702c550938fe269d0cfaa35e45d0be607
David Vazquez <vvdavided@gmail.com>
2019-04-21 16:44:23 -0500

    Remove libraries

--- src/tools/DBTools.java
+++ src/tools/DBTools.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class DBTools {
 
