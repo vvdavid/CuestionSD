840f16c661f5d692fa54de804bd0dbdbd6e02bed
David Vazquez <vvdavided@gmail.com>
2019-04-22 10:30:44 -0500

    Create ExamenJDBC.java

---
+++ src/jdbc/ExamenJDBC.java
@@ -0,0 +1,31 @@
+package jdbc;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import tools.DBTools;
+
+public class ExamenJDBC {
+
+    public static void cargaTabla(JTable tablaExamenJT) {
+        try (
+                PreparedStatement ps = DBTools.getConnection().prepareStatement(
+                        "SELECT * FROM examen");
+                ResultSet rs = ps.executeQuery();) {
+            DefaultTableModel model = (DefaultTableModel) tablaExamenJT.getModel();
+            while (rs.next()) {
+                model.addRow(new Object[]{
+                    rs.getInt(1),
+                    rs.getString(2),
+                    rs.getBoolean(3)
+                });
+            }
+            System.out.println(model);
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
+    }
+
+}
