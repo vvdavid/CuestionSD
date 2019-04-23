83c3475c83940d975cd7deab9e21198092d73738
David Vazquez <vvdavided@gmail.com>
2019-04-23 11:57:00 -0500

    Reinicia tabla

--- src/jdbc/ExamenJDBC.java
+++ src/jdbc/ExamenJDBC.java
@@ -15,6 +15,7 @@ public class ExamenJDBC {
                         "SELECT * FROM examen");
                 ResultSet rs = ps.executeQuery();) {
             DefaultTableModel model = (DefaultTableModel) tablaExamenJT.getModel();
+            model.setRowCount(0);
             while (rs.next()) {
                 model.addRow(new Object[]{
                     rs.getInt(1),
