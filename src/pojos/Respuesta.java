69d43726f81aab83380ad8fc1270dcedc87fa8f4
David Vazquez <vvdavided@gmail.com>
2019-05-13 13:19:13 -0500

    Create Respuesta.java

---
+++ src/pojos/Respuesta.java
@@ -0,0 +1,36 @@
+package pojos;
+
+public class Respuesta {
+
+    private int id;
+    private int idReactivo;
+    private String descripcion;
+    private boolean correcta;
+
+    public Respuesta(int id, int idReactivo, String descripcion, boolean correcta) {
+        this.id = id;
+        this.idReactivo = idReactivo;
+        this.descripcion = descripcion;
+        this.correcta = correcta;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getIdReactivo() {
+        return idReactivo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isCorrecta() {
+        return correcta;
+    }
+
+    public void setIdReactivo(int idReactivo) {
+        this.idReactivo = idReactivo;
+    }
+}
