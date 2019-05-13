55c5785bd2ba5d91ee54c81cd1dbce029857e917
David Vazquez <vvdavided@gmail.com>
2019-05-13 14:38:37 -0500

    Add agrega() in RespuestaJDBC

--- src/jdbc/RespuestaJDBC.java
+++ src/jdbc/RespuestaJDBC.java
@@ -4,6 +4,8 @@ import gui.adminPanels.tipoPaneles.SetRespuestas;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import pojos.Respuesta;
 import tools.DBTools;
 
 public class RespuestaJDBC {
@@ -21,6 +23,23 @@ public class RespuestaJDBC {
                 } else {
                     panel.addIncorrecta(currentDescripcion);
                 }
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+    }
+
+    public static void agrega(ArrayList<Respuesta> respuestas) {
+        try {
+            for (Respuesta respuesta : respuestas) {
+                PreparedStatement ps = DBTools.getConnection().prepareStatement(
+                        "INSERT INTO Respuesta (idReactivo, descripcion, correcta)"
+                        + " VALUES (?, ?, ?)");
+                ps.setInt(1, respuesta.getIdReactivo());
+                ps.setString(2, respuesta.getDescripcion());
+                ps.setBoolean(3, respuesta.isCorrecta());
+                ps.executeUpdate();
+                ps.close();
             }
         } catch (SQLException e) {
             System.err.println(e);
