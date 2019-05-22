685cd196a1092b6818899b233f3ee09e9e231ba1
David Vazquez <vvdavided@gmail.com>
2019-05-22 13:21:10 -0500

    Update Menu

--- src/gui/Menu.java
+++ src/gui/Menu.java
@@ -3,16 +3,44 @@ package gui;
 import com.alee.laf.WebLookAndFeel;
 import com.alee.laf.optionpane.WebOptionPane;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
 import javax.swing.JMenuItem;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import jdbc.ExamenJDBC;
+import jdbc.ReactivoJDBC;
+import pojos.Examen;
 import pojos.Usuario;
 import tools.GUITools;
 
 public class Menu extends javax.swing.JFrame {
 
+    private Driver driver;
+
     public Menu(Usuario usuario) {
         initComponents();
+        driver = new Driver();
+        addListeners();
+
         rangoRS.setBackground(Color.getColor("#f0ecec"));
         nombreJL.setText(usuario.getNombre());
+    }
+
+    private void addListeners() {
+        rendimientoJB.addActionListener(driver);
+        comenzarJB.addActionListener(driver);
+        examenesJCB.addItemListener(driver);
+        rangoRS.addMouseListener(driver);
+        desdeJS.addChangeListener(driver);
+        hastaJS.addChangeListener(driver);
     }
 
     @SuppressWarnings("unchecked")
@@ -26,14 +54,14 @@ public class Menu extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         mimimoJL = new javax.swing.JLabel();
         actualLowJL = new javax.swing.JLabel();
-        desdeJL = new javax.swing.JLabel();
         aleatorioJCB = new javax.swing.JCheckBox();
         comenzarJB = new javax.swing.JButton();
         rangoRS = new slider.RangeSlider();
         jScrollPane1 = new javax.swing.JScrollPane();
         reactivosJT = new javax.swing.JTable();
         actualLowJL1 = new javax.swing.JLabel();
-        hastJL = new javax.swing.JLabel();
+        desdeJS = new javax.swing.JSpinner();
+        hastaJS = new javax.swing.JSpinner();
         jPanel3 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
         historialJT = new javax.swing.JTable();
@@ -54,6 +82,11 @@ public class Menu extends javax.swing.JFrame {
 
         cerrarSesionJB.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/logout.png"))); // NOI18N
         cerrarSesionJB.setText("Cerrar sesión");
+        cerrarSesionJB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cerrarSesionJBActionPerformed(evt);
+            }
+        });
 
         jLabel1.setText("Exámen:");
 
@@ -62,19 +95,14 @@ public class Menu extends javax.swing.JFrame {
         actualLowJL.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
         actualLowJL.setText("Desde:");
 
-        desdeJL.setText("999");
-
         aleatorioJCB.setText("Orden aleatorio");
 
         comenzarJB.setText("Comenzar");
 
-        rangoRS.setMajorTickSpacing(20);
+        rangoRS.setMajorTickSpacing(10);
         rangoRS.setMaximum(500);
-        rangoRS.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseReleased(java.awt.event.MouseEvent evt) {
-                rangoRSMouseReleased(evt);
-            }
-        });
+        rangoRS.setMinimum(1);
+        rangoRS.setPaintTicks(true);
 
         reactivosJT.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -83,11 +111,11 @@ public class Menu extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Selección", "ID", "Descripción", "Tipo"
+                "Selección", "Número", "Descripción", "Tipo"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Boolean.class, java.lang.Integer.class, java.lang.String.class, java.lang.Object.class
+                java.lang.Boolean.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
                 true, false, false, false
@@ -101,16 +129,22 @@ public class Menu extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
+        reactivosJT.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_ALL_COLUMNS);
         reactivosJT.getTableHeader().setReorderingAllowed(false);
         jScrollPane1.setViewportView(reactivosJT);
         if (reactivosJT.getColumnModel().getColumnCount() > 0) {
-            reactivosJT.getColumnModel().getColumn(0).setMaxWidth(80);
+            reactivosJT.getColumnModel().getColumn(0).setPreferredWidth(80);
+            reactivosJT.getColumnModel().getColumn(1).setPreferredWidth(80);
+            reactivosJT.getColumnModel().getColumn(2).setPreferredWidth(400);
+            reactivosJT.getColumnModel().getColumn(3).setPreferredWidth(150);
         }
 
         actualLowJL1.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
         actualLowJL1.setText("Hasta:");
 
-        hastJL.setText("999");
+        desdeJS.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
+
+        hastaJS.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -126,20 +160,21 @@ public class Menu extends javax.swing.JFrame {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(examenesJCB, 0, 844, Short.MAX_VALUE))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 905, Short.MAX_VALUE)
+                        .addComponent(examenesJCB, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(mimimoJL)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(rangoRS, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(rangoRS, javax.swing.GroupLayout.PREFERRED_SIZE, 535, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
                         .addComponent(actualLowJL)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(desdeJL)
+                        .addComponent(desdeJS, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addComponent(actualLowJL1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(hastJL)))
+                        .addComponent(hastaJS, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -152,7 +187,7 @@ public class Menu extends javax.swing.JFrame {
                             .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(examenesJCB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 615, Short.MAX_VALUE)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 611, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(mimimoJL)
@@ -161,9 +196,9 @@ public class Menu extends javax.swing.JFrame {
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(actualLowJL, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(desdeJL, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(actualLowJL1, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(hastJL, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(desdeJS, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(hastaJS, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(aleatorioJCB)
@@ -358,13 +393,13 @@ public class Menu extends javax.swing.JFrame {
                 WebOptionPane.INFORMATION_MESSAGE);
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
-    private void rangoRSMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_rangoRSMouseReleased
-        System.out.println("Actualizar ids seleccionados");
-    }//GEN-LAST:event_rangoRSMouseReleased
-
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         panelJTP.setSelectedIndex(0);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void cerrarSesionJBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cerrarSesionJBActionPerformed
+        GUITools.openJFrame(this, new Login());
+    }//GEN-LAST:event_cerrarSesionJBActionPerformed
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -382,11 +417,11 @@ public class Menu extends javax.swing.JFrame {
     private javax.swing.JLabel bienvenidaJL;
     private javax.swing.JButton cerrarSesionJB;
     private javax.swing.JButton comenzarJB;
-    private javax.swing.JLabel desdeJL;
-    private javax.swing.JComboBox<String> examenesJCB;
+    private javax.swing.JSpinner desdeJS;
+    private javax.swing.JComboBox<Examen> examenesJCB;
     private javax.swing.JPanel grafica1JP;
     private javax.swing.JPanel grafica2JP;
-    private javax.swing.JLabel hastJL;
+    private javax.swing.JSpinner hastaJS;
     private javax.swing.JTable historialJT;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMenu3;
@@ -406,4 +441,64 @@ public class Menu extends javax.swing.JFrame {
     private javax.swing.JTable reactivosJT;
     private javax.swing.JButton rendimientoJB;
     // End of variables declaration//GEN-END:variables
+
+    private class Driver extends MouseAdapter implements ActionListener, ItemListener, ChangeListener {
+
+        public Driver() {
+            ExamenJDBC.cargaComboActivos(examenesJCB);
+            cargaReactivos();
+            inicializaSliderYSpinners();
+        }
+
+        //actualizar datos
+        private void cargaReactivos() {
+            ReactivoJDBC.cargaReactivosTest(reactivosJT, ((Examen) examenesJCB.getSelectedItem()).getId());
+        }
+
+        private void inicializaSliderYSpinners() {
+            int max = reactivosJT.getRowCount();
+
+            rangoRS.setMaximum(max);
+            rangoRS.setValue(1);
+            rangoRS.setUpperValue(max);
+
+            desdeJS.setModel(new SpinnerNumberModel(1, 1, max, 1));
+            hastaJS.setModel(new SpinnerNumberModel(max, 1, max, 1));
+            desdeJS.setValue(1);
+            hastaJS.setValue(max);
+        }
+
+        //acciones
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            JButton src = (JButton) ae.getSource();
+            if (src == comenzarJB) {
+                System.out.println("comenzar");
+            } else if (src == rendimientoJB) {
+                System.out.println("rendimiento");
+            } else {
+                throw new UnsupportedOperationException();
+            }
+        }
+
+        @Override
+        public void itemStateChanged(ItemEvent ie) {
+            cargaReactivos();
+            inicializaSliderYSpinners();
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent me) {
+            desdeJS.setValue(rangoRS.getValue());
+            hastaJS.setValue(rangoRS.getUpperValue());
+
+            //
+        }
+
+        @Override
+        public void stateChanged(ChangeEvent ce) {
+            rangoRS.setValue((int) desdeJS.getValue());
+            rangoRS.setUpperValue((int) hastaJS.getValue());
+        }
+    }
 }
