c08779599021fdcefdff17358bc2ff024d848ca8
David Vazquez <vvdavided@gmail.com>
2019-05-27 01:20:48 -0500

    Create Responsable interface

---
+++ src/gui/TestPanels/Responsable.java
@@ -0,0 +1,11 @@
+package gui.TestPanels;
+
+import java.util.ArrayList;
+import pojos.Respuesta;
+
+public interface Responsable {
+
+    void cargaRespuestas(ArrayList<Respuesta> respuestas);
+
+//    boolean esCorrecta();
+}
